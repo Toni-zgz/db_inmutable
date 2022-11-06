ab417ddb3df5b5cf3b0791063f5557b87343b608
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2022-11-06 11:37:33 +0100

    Proyecto inicial

---
+++ src/db_inmutable/core.clj
@@ -0,0 +1,6 @@
+(ns db-inmutable.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/db_inmutable/core_test.clj
@@ -0,0 +1,7 @@
+(ns db-inmutable.core-test
+  (:require [clojure.test :refer :all]
+            [db-inmutable.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
