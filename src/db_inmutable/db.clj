c14fb46f62f4f6aaa275026b51c34385b1564b73
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2022-11-07 20:09:05 +0100

    Incluyo las funciones insertar y su función helper  esta-en y los test suite de guardar-a-disco,  recuperar-de-disco, mover-elemento e insertar

--- src/db_inmutable/db.clj
+++ src/db_inmutable/db.clj
@@ -1,4 +1,5 @@
-(ns db-inmutable.db)
+(ns db-inmutable.db
+  (:require [db-inmutable.io :as io]))
 
 ; reordenar-lista :: (Map) -> Long -> Long -> (Map)
 (defn mover-elemento [lista origen destino]
@@ -20,3 +21,37 @@
                               (rest entrada))
               nueva-salida (conj salida nodo-modificado)]
         (recur nuevo-indice nueva-entrada nueva-salida))))))
+
+; esta-en :: Map -> List Map (Long Long Map (String Long)) -> Bool
+(defn- esta-en [elt lista clave]
+  (cond (not (map? elt)) (println "El primer argumento de la función esta-en debe ser un diccionario.")
+        (not (list? lista)) (println "El segundo argumento de la función esta-en debe ser una lista.")
+        (not (keyword? clave)) (println "El tercer argumento de la función esta-en debe ser un keyword.")
+        :else (if (= lista '())
+                false
+                (let [filtro (fn [elt-filter]
+                               (-> (get elt-filter clave)
+                                   (= elt)))]
+                  (not (= (filter filtro lista) '()))))))
+
+; insertar :: Map -> (Map) -> String -> (Map)
+(defn insertar [nuevo-dato datos nombre-fichero]
+  (cond (not (map? nuevo-dato)) (println "El primer parámetro debe ser un diccionario.")
+        (not (list? datos)) (println "El segundo parámetro debe ser una lista.")
+        (not (string? nombre-fichero)) (println "El tercer parámetro debe ser una cadena.")
+        (esta-en nuevo-dato datos :value) (println "El nuevo dato ya está en la lista de datos.")
+        :else (let [ultimo-indice (if (= datos '())
+                                    0
+                                    (reduce (fn [acc elt]
+                                              (max acc (get elt :id)))
+                                            0
+                                            datos))
+                    nuevo-indice (inc ultimo-indice)
+                    nuevo-elemento {:id nuevo-indice,
+                                    :rev 0
+                                    :value nuevo-dato}
+                    array-antiguo (into [] datos)
+                    nuevo-array (conj array-antiguo nuevo-elemento)
+                    nueva-lista (reverse (into '() nuevo-array))]
+                (io/guardar-a-disco nueva-lista nombre-fichero)
+                nueva-lista)))
--- test/db_inmutable/core_test.clj
+++ test/db_inmutable/core_test.clj
@@ -1,7 +1,41 @@
 (ns db-inmutable.core-test
-  (:require [clojure.test :refer :all]
-            [db-inmutable.core :refer :all]))
+  (:require [clojure.test :as test]
+            [db-inmutable.core :refer :all]
+            [db-inmutable.io :as io]
+            [db-inmutable.db :as db]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(test/testing "Tests unitarios de db-inmutable/io"
+  (let [entrada '({:id 1, :value {:nombre "Blanca", :edad 34}, :version 0}
+                  {:id 2, :value {:nombre "Juan", :edad 22}, :version 0})
+        nombre-fichero "test.fiabledb"]
+    (io/guardar-a-disco entrada nombre-fichero)
+    (let [salida (io/recuperar-de-disco nombre-fichero)]
+      (test/is (= salida entrada)))))
+
+(test/testing "Tests unitarios de db-inmutable/io"
+  (let [lista1 '({:id 1 :idioma :ingles}
+                 {:id 2 :idioma :frances}
+                 {:id 3 :idioma :aleman}
+                 {:id 4 :idioma :italiano}
+                 {:id 5 :idioma :portugues}
+                 {:id 6 :idioma :ruso})
+        lista2 '({:id 1 :idioma :ingles}
+                 {:id 2 :idioma :aleman}
+                 {:id 3 :idioma :italiano}
+                 {:id 4 :idioma :portugues}
+                 {:id 5 :idioma :frances}
+                 {:id 6 :idioma :ruso})
+        lista3 '({:id 1 :idioma :ingles}
+                 {:id 2 :idioma :aleman}
+                 {:id 3 :idioma :italiano}
+                 {:id 4 :idioma :portugues}
+                 {:id 5 :idioma :ruso}
+                 {:id 6 :idioma :frances})
+        lista4 '()
+        lista5 '({:id 1, :rev 0, :value {:nombre "Alvaro", :edad 35}})
+        lista6 '({:id 1, :rev 0, :value {:nombre "Alvaro", :edad 35}}
+                 {:id 2, :rev 0, :value {:nombre "Rosa", :edad 50}})]
+    (test/is (= (db/mover-elemento lista1 2 5) lista2))
+    (test/is (= (db/mover-elemento lista1 2 6) lista3))
+    (test/is (= (db/insertar {:nombre "Alvaro", :edad 35} lista4 "test.fiabledb") lista5))
+    (test/is (= (db/insertar {:nombre "Rosa", :edad 50} lista5 "test.fiabledb") lista6))))
